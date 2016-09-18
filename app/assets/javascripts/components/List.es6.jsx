08636d2c4482120ed4ee50a663fe9af43b7cdcbb
Elisa Verna <email": null>
2016-09-18 13:24:40 -0400

    added notes, made it so list items disspear after drag

--- app/assets/javascripts/components/List.es6.jsx
+++ app/assets/javascripts/components/List.es6.jsx
@@ -3,21 +3,29 @@ class List extends React.Component {
     super()
     this.state = {
       items: ['cat', 'dog', 'chicken', 'bunny', 'cow', 'horse', 'walrus']
+      // array of list items
     }
   }
-
+  // begin the drag
   dragStart(e){
     this.dragged = e.currentTarget;
+    // defines what is being dragged
     e.dataTransfer.effectAllowed = 'move';
+    // specifies the effect that is allowed for a drag operation
     e.dataTransfer.setData('text/html', e.currentTarget);
+    // sets the drag operation's drag data to the specified data and type (format, type)
   }
 
+  // complete the drag
   dragEnd(e) {
-
+    e.preventDefault();
+    this.dragged.style.display = "none";
   }
 
+  // choose how things are displayed after drag has completed
   dragOver(e){
-
+    e.preventDefault();
+    this.dragged.style.display = "none";
   }
 
   render() {
@@ -25,8 +33,9 @@ class List extends React.Component {
       <ul onDragOver={this.dragOver}>
       {
        this.state.items.map((item, index) => {
-         return (<li key={index} data-id={index} draggable="true" onDragEnd={this.dragEnd} onDragStart={this.dragStart}>{item}</li>)
+         return (<li id="target" key={index} data-id={index} draggable="true" onDragEnd={this.dragEnd} onDragStart={this.dragStart}>{item}</li>)
        })
+       // to make an element draggable, set draggable attribute to true
      }
       </ul>
     )
